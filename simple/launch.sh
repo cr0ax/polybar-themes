acd1658ca240d7d412d79870db2bbb631560179b
cr0ax <cr0ax64@gmail.com>
2022-02-05 10:06:08 -0800

    Hack for multi-monitor

--- simple/launch.sh
+++ simple/launch.sh
@@ -10,14 +10,13 @@ launch_bar() {
 	# Wait until the processes have been shut down
 	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-	# Launch the bar
 	if [[ "$style" == "hack" || "$style" == "cuts" ]]; then
-		polybar -q top -c "$dir/$style/config.ini" &
-		polybar -q bottom -c "$dir/$style/config.ini" &
+		MONITOR=$m polybar -q top -c "$dir/$style/config.ini" &
+		MONITOR=$m polybar -q bottom -c "$dir/$style/config.ini" &
 	elif [[ "$style" == "pwidgets" ]]; then
 		bash "$dir"/pwidgets/launch.sh --main
 	else
-		polybar -q main -c "$dir/$style/config.ini" &	
+		MONITOR=$m polybar -q main -c "$dir/$style/config.ini" &
 	fi
 }
 
